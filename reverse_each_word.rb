d66b3cd60fe473cad01d8215d82d13f33bd3fcfd
Eliot Howes <ejhowes@gmail.com>
2019-06-10 21:15:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}
+  reversed = sentence.split.collect {|word| word.reverse}
   reversed.join(" ")
 end
