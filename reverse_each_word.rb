3f69b1713ef4cd92adcdbb827d5e86b783809c38
Eliot Howes <ejhowes@gmail.com>
2019-06-10 21:11:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def reverse_each_word(sentence)
   reversed = []
-  reverse << sentence.split.each {|word| word.reverse}
+  reversed << sentence.split.each {|word| word.reverse}
   binding.pry
 end
